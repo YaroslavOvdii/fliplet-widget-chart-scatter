97392cbb426f1ce49cc323e3f9e847b3ddd89442
Tony Wu <Tony@Tony.home>
2017-05-21 23:27:16 +0100

    Updated to use data source query provider

--- js/build.js
+++ js/build.js
@@ -1,172 +1,186 @@
 window.ui = window.ui || {}
-ui.flipletCharts = {};
-function init(){
-  Fliplet.Navigator.onReady().then(function(){
-    $('[data-chart-scatter-id]').each(function (i, el) {
-      var chartId = $(this).data('chart-scatter-id');
-      var data = Fliplet.Widget.getData( chartId );
-      var $container = $(el);
-      var refreshTimeout = 5000;
-      // var updateDateFormat = 'MMMM Do YYYY, h:mm:ss a';
-      var updateDateFormat = 'hh:mm:ss a';
+ui.flipletCharts = ui.flipletCharts || {};
+Fliplet().then(function(){
+  $('[data-chart-scatter-id]').each(function (i, el) {
+    var chartId = $(this).data('chart-scatter-id');
+    var data = Fliplet.Widget.getData( chartId );
+    var $container = $(el);
+    var refreshTimeout = 5000;
+    var updateDateFormat = 'hh:mm:ss a';
 
-      function resetData() {
+    function resetData() {
+      data.entries = [];
+      data.totalEntries = 0;
+    }
+
+    function refreshData() {
+      return Fliplet.DataSources.fetchWithOptions(data.dataSourceQuery).then(function(result){
         data.entries = [];
         data.totalEntries = 0;
-      }
-
-      function refreshData() {
-        return Fliplet.DataSources.fetchWithOptions({
-          dataSourceId: parseInt(data.dataSourceId, 10),
-          columns: [data.dataSourceColumnX, data.dataSourceColumnY]
-        }).then(function(result){
-          data.entries = [];
-          data.totalEntries = 0;
-          if (result.dataSource.columns.indexOf(data.dataSourceColumnX) < 0 || result.dataSource.columns.indexOf(data.dataSourceColumnY) < 0) {
-            return Promise.resolve();
-          }
-          result.dataSourceEntries.forEach(function(row) {
-            var x;
-            if (data.dataFormat === 'timestamp') {
-              x = new Date(row[data.dataSourceColumnX] || 0).getTime()
-            } else {
-              x = parseInt(row[data.dataSourceColumnX], 10) || 0;
-            }
-            var y = parseInt(row[data.dataSourceColumnY], 0) || 0;
-            data.entries.push([x, y]);
-          });
-          // SAVES THE TOTAL NUMBER OF ROW/ENTRIES
-          data.totalEntries = data.entries.length;
-
+        if (result.dataSource.columns.indexOf(data.dataSourceQuery.columns.xAxis) < 0 || result.dataSource.columns.indexOf(data.dataSourceQuery.columns.yAxis) < 0) {
           return Promise.resolve();
+        }
+        result.dataSourceEntries.forEach(function(row) {
+          var x;
+          if (data.dataFormat === 'timestamp') {
+            x = new Date(row[data.dataSourceQuery.columns.xAxis] || 0).getTime()
+          } else {
+            x = parseInt(row[data.dataSourceQuery.columns.xAxis], 10) || 0;
+          }
+          var y = parseInt(row[data.dataSourceQuery.columns.yAxis], 0) || 0;
+          data.entries.push([x, y]);
         });
-      }
+        // SAVES THE TOTAL NUMBER OF ROW/ENTRIES
+        data.totalEntries = data.entries.length;
 
-      function refreshChartInfo() {
-        // Update total count
-        $container.find('.total').html(data.totalEntries);
-        // Update last updated time
-        $container.find('.updatedAt').html(moment().format(updateDateFormat));
-      }
+        return Promise.resolve();
+      });
+    }
 
-      function refreshChart() {
-        // Retrieve chart object
-        var chart = ui.flipletCharts[chartId];
-        // Update values
-        chart.series[0].setData(data.entries);
-        refreshChartInfo();
-      }
+    function refreshChartInfo() {
+      // Update total count
+      $container.find('.total').html(data.totalEntries);
+      // Update last updated time
+      $container.find('.updatedAt').html(moment().format(updateDateFormat));
+    }
 
-      function getLatestData() {
-        setTimeout(function(){
-          refreshData().then(function(){
-            refreshChart();
-            if (data.autoRefresh) {
-              getLatestData();
-            }
-          });
-        }, refreshTimeout);
-      }
+    function refreshChart() {
+      // Retrieve chart object
+      var chart = ui.flipletCharts[chartId];
+      // Update values
+      chart.series[0].setData(data.entries);
+      refreshChartInfo();
+    }
 
-      function drawChart() {
-        var chartOpt = {
-          chart: {
-            type: 'scatter',
-            zoomType: 'xy',
-            renderTo: $container.find('.chart-scatter-container')[0],
-            events: {
-              load: function(){
-                refreshChartInfo();
-                if (data.autoRefresh) {
-                  getLatestData();
-                }
+    function getLatestData() {
+      setTimeout(function(){
+        refreshData().then(function(){
+          refreshChart();
+          if (data.autoRefresh) {
+            getLatestData();
+          }
+        });
+      }, refreshTimeout);
+    }
+
+    function drawChart() {
+      var colors = [
+        '#337AB7', '#5BC0DE', '#5CB85C', '#F0AD4E', '#C9302C',
+        '#293954', '#2E6F82', '#3D7A3D', '#B07623', '#963732'
+      ];
+      colors.forEach(function eachColor (color, index) {
+        if (!Fliplet.Themes) {
+          return;
+        }
+        colors[index] = Fliplet.Themes.Current.get('chartColor'+(index+1)) || color;
+      });
+      var chartOpt = {
+        chart: {
+          type: 'scatter',
+          zoomType: 'xy',
+          renderTo: $container.find('.chart-scatter-container')[0],
+          style: {
+            fontFamily: (Fliplet.Themes && Fliplet.Themes.Current.get('bodyFontFamily')) || 'sans-serif'
+          },
+          events: {
+            load: function(){
+              refreshChartInfo();
+              if (data.autoRefresh) {
+                getLatestData();
               }
             }
-          },
+          }
+        },
+        colors: colors,
+        title: {
+          text: ''
+        },
+        subtitle: {
+          text: ''
+        },
+        xAxis: {
           title: {
-            text: ''
+            text: data.xAxisTitle || data.dataSourceQuery.xAxis,
+            enabled: data.xAxisTitle !== ''
           },
-          subtitle: {
-            text: ''
-          },
-          xAxis: {
-            title: {
-              text: data.xAxisTitle,
-              enabled: data.xAxisTitle !== ''
-            },
-            labels: {
-              formatter: function(){
-                if (data.dataFormat === 'timestamp') {
-                  return moment(this.value).format('YYYY-MM-DD');
-                }
-                return this.value;
+          labels: {
+            formatter: function(){
+              if (data.dataFormat === 'timestamp') {
+                return moment(this.value).format('YYYY-MM-DD');
               }
-            },
-            startOnTick: true,
-            endOnTick: true,
-            showLastLabel: true
-          },
-          yAxis: {
-            title: {
-              text: data.yAxisTitle,
-              enabled: data.yAxisTitle !== ''
+              return this.value;
             }
           },
-          tooltip: {
-            enabled: data.showDataValues,
-            headerFormat: '',
-            pointFormat: [
-              '<strong>',
-              (data.xAxisTitle !== '' ? data.xAxisTitle : 'x'),
-              '</strong> ',
-              (data.dataFormat === 'timestamp' ? '{point.x:%Y-%m-%d %H:%M:%S}' : '{point.x}'),
-              '<br><strong>',
-              (data.yAxisTitle !== '' ? data.yAxisTitle : 'y'),
-              '</strong>: {point.y}'
-            ].join('')
-          },
-          plotOptions: {
-            scatter: {
-              marker: {
-                radius: 5,
-                states: {
-                  hover: {
-                    enabled: true,
-                    lineColor: 'rgb(100,100,100)'
-                  }
-                }
-              },
+          startOnTick: true,
+          endOnTick: true,
+          showLastLabel: true
+        },
+        yAxis: {
+          title: {
+            text: data.yAxisTitle || data.dataSourceQuery.yAxis,
+            enabled: data.yAxisTitle !== ''
+          }
+        },
+        tooltip: {
+          enabled: data.showDataValues,
+          headerFormat: '',
+          pointFormat: [
+            '<strong>',
+            (data.xAxisTitle !== ''
+              ? data.xAxisTitle
+              : data.dataSourceQuery.xAxis),
+            '</strong> ',
+            (data.dataFormat === 'timestamp'
+              ? '{point.x:%Y-%m-%d %H:%M:%S}'
+              : '{point.x}'),
+            '<br><strong>',
+            (data.yAxisTitle !== ''
+              ? data.yAxisTitle
+              : data.dataSourceQuery.yAxis),
+            '</strong>: {point.y}'
+          ].join('')
+        },
+        plotOptions: {
+          scatter: {
+            marker: {
+              radius: 5,
               states: {
                 hover: {
-                  marker: {
-                    enabled: false
-                  }
+                  enabled: true,
+                  lineColor: 'rgb(100,100,100)'
+                }
+              }
+            },
+            states: {
+              hover: {
+                marker: {
+                  enabled: false
                 }
               }
             }
-          },
-          series: [{
-            data: data.entries,
-            color: '#3276b1'
-          }],
-          legend: {
-            enabled: false
           }
-        };
-        // Create and save chart object
-        ui.flipletCharts[chartId] = new Highcharts.Chart(chartOpt);
-      }
+        },
+        series: [{
+          data: data.entries
+        }],
+        legend: {
+          enabled: false
+        },
+        credits: {
+          enabled: false
+        }
+      };
+      // Create and save chart object
+      ui.flipletCharts[chartId] = new Highcharts.Chart(chartOpt);
+    }
 
-      refreshData().then(drawChart);
-    });
+    refreshData().then(drawChart);
   });
-}
+  var debounceLoad = _.debounce(init, 500);
 
-var debounceLoad = _.debounce(init, 500);
-
-Fliplet.Studio.onEvent(function (event) {
-  if (event.detail.event === 'reload-widget-instance') {
-    debounceLoad();
-  }
+  Fliplet.Studio.onEvent(function (event) {
+    if (event.detail.event === 'reload-widget-instance') {
+      debounceLoad();
+    }
+  });
 });
-init();
--- js/interface.js
+++ js/interface.js
@@ -1,4 +1,5 @@
 var data = Fliplet.Widget.getData() || {
+  dataSourceQuery: undefined,
   showDataValues: true,
   dataFormat: 'number',
   yAxisTitle: '',
@@ -7,21 +8,36 @@ var data = Fliplet.Widget.getData() || {
   autoRefresh: ''
 };
 
-var $dataSource = $('select#select-data-source');
-var $dataColumnX = $('select#select-data-column-x');
-var $dataColumnY = $('select#select-data-column-y');
+var dsQueryData = {
+  settings: {
+    dataSourceLabel: 'Select a data source',
+    filters: false,
+    columns: [
+      {
+        key: 'xAxis',
+        label: 'Select a column for the X-axis',
+        type: 'single'
+      },
+      {
+        key: 'yAxis',
+        label: 'Select a column for the Y-axis',
+        type: 'single'
+      }
+    ]
+  },
+  result: data.dataSourceQuery
+};
+
 var $dataFormat = $('select#select-data-format');
-var organizationId = Fliplet.Env.get('organizationId');
-var initialised = false;
 
-// Fired from Fliplet Studio when the external save button is clicked
-Fliplet.Widget.onSaveRequest(function () {
-  if (!initialised) return Fliplet.Widget.complete();
+var dsQueryProvider = Fliplet.Widget.open('com.fliplet.data-source-query', {
+  selector: '.data-source-query',
+  data: dsQueryData
+});
 
+dsQueryProvider.then(function(result){
   Fliplet.Widget.save({
-    dataSourceId: parseInt($dataSource.val(), 10),
-    dataSourceColumnX: $dataColumnX.val(),
-    dataSourceColumnY: $dataColumnY.val(),
+    dataSourceQuery: result.data,
     dataFormat: $dataFormat.find(':selected').val(),
     showDataValues: $('#show_data_values:checked').val() === "show",
     yAxisTitle: $('#y_axis_title').val(),
@@ -30,32 +46,13 @@ Fliplet.Widget.onSaveRequest(function () {
     autoRefresh: $('#auto_refresh:checked').val() === "refresh"
   }).then(function () {
     Fliplet.Widget.complete();
+    Fliplet.Studio.emit('reload-page-preview');
   });
 });
 
-// GET DATA SOURCES, EXCLUDE MENUS, GET ALL THE ROWS
-Fliplet.DataSources.get({
-  organizationId: organizationId
-}).then(function (dataSources) {
-  data.dataSources = dataSources;
-  var templateSource = $('template[name="dataSourceTemplate"]').html();
-  var template = Handlebars.compile(templateSource);
-  $dataSource.html(template(dataSources));
-
-  // LOADS DATA SOURCE DATA
-  // NEEDS TO BE DONE HERE BECAUSE THE SELECT BOX NEEDS TO BE DYNAMIC UPDATED
-  if (data.dataSourceId) {
-    $dataSource.val(data.dataSourceId);
-    showColumnSelect();
-  }
-  $dataSource.trigger('change');
-
-  if ( data.dataSourceId ) {
-    $dataColumnX.val(data.dataSourceColumnX).trigger('change');
-    $dataColumnY.val(data.dataSourceColumnY).trigger('change');
-  }
-
-  initialised = true;
+// Fired from Fliplet Studio when the external save button is clicked
+Fliplet.Widget.onSaveRequest(function () {
+  dsQueryProvider.forwardSaveRequest();
 });
 
 // LOAD CHART SETTINGS
@@ -66,51 +63,4 @@ if (data) {
   $('#x_axis_title').val(data.xAxisTitle);
   $('#show_total_entries').prop('checked', data.showTotalEntries);
   $('#auto_refresh').prop('checked', data.autoRefresh);
-}
-
-// ATTACH LISTENERS
-$dataSource.on('change', function(){
-  var selectedValue = $(this).val();
-  var selectedText = $(this).find("option:selected").text();
-
-  var templateSource = $('template[name="dataColumnTemplate"]').html();
-  var template = Handlebars.compile(templateSource);
-
-  var dataSource = _.find(data.dataSources, {id: parseInt(selectedValue,10)});
-  if (typeof dataSource !== 'undefined') {
-    $dataColumnX.html(template(dataSource.columns));
-    $dataColumnY.html(template(dataSource.columns));
-    showColumnSelect();
-  }
-
-  $(this).parents('.select-proxy-display').find('.select-value-proxy').html(selectedText);
-  checkDataIsConfigured();
-});
-
-$dataColumnX.add($dataColumnY).on('change', function() {
-  var selectedText = $(this).find("option:selected").text();
-  $(this).parents('.select-proxy-display').find('.select-value-proxy').html(selectedText);
-  checkDataIsConfigured();
-});
-
-$dataFormat.on('change', function(){
-  var selectedText = $(this).find("option:selected").text();
-  $(this).parents('.select-proxy-display').find('.select-value-proxy').html(selectedText);
-});
-
-// FUNCTIONS
-function showColumnSelect() {
-  if ($dataSource.val() !== 'none') {
-    $('.select-data-column').removeClass('hidden');
-  } else {
-    $('.select-data-column').addClass('hidden');
-  }
-}
-
-function checkDataIsConfigured() {
-  if ($dataSource.val() !== '' && $dataColumnX.val() !== '' && $dataColumnY.val() !== '') {
-    $('#chart-settings').removeClass('hidden');
-  } else {
-    $('#chart-settings').addClass('hidden');
-  }
 }
