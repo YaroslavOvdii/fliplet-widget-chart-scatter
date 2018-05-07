6cd4b43fd0bf6b9bc3f100b3fbbc4b4fa71fc253
Tony Wu <twu@fliplet.com>
2018-05-07 16:47:02 +0100

    Allows users to customize chart heights for mobile and tablet/desktop

--- js/interface.js
+++ js/interface.js
@@ -1,12 +1,19 @@
-var data = Fliplet.Widget.getData() || {
+var defaultChartHeight = {
+  sm: '400px',
+  md: '500px'
+};
+var defaultData = {
   dataSourceQuery: undefined,
-  showDataValues: true,
   dataFormat: 'number',
+  chartHeightSm: defaultChartHeight.sm,
+  chartHeightMd: defaultChartHeight.md,
+  showDataValues: true,
   yAxisTitle: '',
   xAxisTitle: '',
-  showTotalEntries: '',
-  autoRefresh: ''
+  showTotalEntries: false,
+  autoRefresh: false
 };
+var data = $.extend(defaultData, Fliplet.Widget.getData());
 
 var dsQueryData = {
   settings: {
@@ -35,29 +42,68 @@ var dsQueryProvider = Fliplet.Widget.open('com.fliplet.data-source-query', {
   data: dsQueryData
 });
 
-dsQueryProvider.then(function(result){
-  Fliplet.Widget.save({
-    dataSourceQuery: result.data,
-    dataFormat: $dataFormat.find(':selected').val(),
-    showDataValues: $('#show_data_values:checked').val() === "show",
-    yAxisTitle: $('#y_axis_title').val(),
-    xAxisTitle: $('#x_axis_title').val(),
-    showTotalEntries: $('#show_total_entries:checked').val() === "show",
-    autoRefresh: $('#auto_refresh:checked').val() === "refresh"
-  }).then(function () {
-    Fliplet.Widget.complete();
-    Fliplet.Studio.emit('reload-page-preview');
-  });
-});
+// Ensure chart heights have a correct default & units
+function validateChartHeight(val, size) {
+  if (typeof val !== 'string') {
+    val = val.toString() || '';
+  }
 
-// Fired from Fliplet Studio when the external save button is clicked
-Fliplet.Widget.onSaveRequest(function () {
-  dsQueryProvider.forwardSaveRequest();
-});
+  if (!val) {
+    // Set empty values to the default
+    val = defaultChartHeight[size];
+  }
+
+  if (parseFloat(val) <= 0) {
+    val = '0px';
+  }
+
+  if (/^\d+$/.test(val)) {
+    // Value contains only numbers
+    val = val + 'px';
+  }
+
+  return val;
+}
+
+function validateForm() {
+  // Validate chart height
+  $('#chart_height_sm').val(validateChartHeight($('#chart_height_sm').val()), 'sm');
+  $('#chart_height_md').val(validateChartHeight($('#chart_height_md').val()), 'md');
+}
+
+function attachObservers() {
+  dsQueryProvider.then(function(result){
+    validateForm();
+    
+    Fliplet.Widget.save({
+      dataSourceQuery: result.data,
+      dataFormat: $dataFormat.find(':selected').val(),
+      chartHeightSm: $('#chart_height_sm').val(),
+      chartHeightMd: $('#chart_height_md').val(),
+      showDataValues: $('#show_data_values:checked').val() === "show",
+      yAxisTitle: $('#y_axis_title').val(),
+      xAxisTitle: $('#x_axis_title').val(),
+      showTotalEntries: $('#show_total_entries').is(':checked'),
+      autoRefresh: $('#auto_refresh').is(':checked')
+    }).then(function () {
+      Fliplet.Widget.complete();
+      Fliplet.Studio.emit('reload-page-preview');
+    });
+  });
+
+  // Fired from Fliplet Studio when the external save button is clicked
+  Fliplet.Widget.onSaveRequest(function () {
+    dsQueryProvider.forwardSaveRequest();
+  });
+}
+
+attachObservers();
 
 // LOAD CHART SETTINGS
 if (data) {
   $dataFormat.val(data.dataFormat || 'number').trigger('change');
+  $('#chart_height_sm').val(data.chartHeightSm);
+  $('#chart_height_md').val(data.chartHeightMd);
   $('#show_data_values').prop('checked', data.showDataValues);
   $('#y_axis_title').val(data.yAxisTitle);
   $('#x_axis_title').val(data.xAxisTitle);
