a500bf633035dc06f2deaaadfed578a9172d03ef
hcarneiro <hcarneiro@weboo.biz>
2019-06-12 10:42:18 +0100

    Listening to styles changed hook

--- js/build.js
+++ js/build.js
@@ -225,6 +225,10 @@
         $(this).find('.chart-styles').remove();
       }
 
+      Fliplet.Hooks.on('appearanceChanged', () => {
+        ui.flipletCharts[chartId].reflow();
+      });
+
       refreshData().then(drawChart).catch(function(error){
         console.error(error);
       });
