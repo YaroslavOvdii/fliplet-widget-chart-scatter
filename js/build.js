ebf8beefb247d97bdc494f194a8ff4d8efc1f27c
hcarneiro <hcarneiro@weboo.biz>
2019-06-12 11:02:30 +0100

    No arrow functions

--- js/build.js
+++ js/build.js
@@ -225,7 +225,7 @@
         $(this).find('.chart-styles').remove();
       }
 
-      Fliplet.Hooks.on('appearanceChanged', () => {
+      Fliplet.Hooks.on('appearanceChanged', function () {
         ui.flipletCharts[chartId].reflow();
       });
 
