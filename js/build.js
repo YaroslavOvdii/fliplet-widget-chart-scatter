e1e4f8629c5dc7642be6e894f71b78464cb3b1b8
YaroslavOvdii <yaroslav.o@upplabs.com>
2020-01-27 16:11:08 +0200

    Changed to instance

--- js/build.js
+++ js/build.js
@@ -3,10 +3,9 @@
   ui.flipletCharts = ui.flipletCharts || {};
 
   function init() {
-    $('[data-chart-scatter-id]').each(function (i, el) {
-      var chartId = $(this).data('chart-scatter-id');
-      var data = Fliplet.Widget.getData(chartId);
-      var $container = $(el);
+    Fliplet.Widget.instance('chart-scatter', function (data) {
+      var chartId = data.id;
+      var $container = $(this);
       var refreshTimeout = 5000;
       var updateDateFormat = 'hh:mm:ss a';
 
