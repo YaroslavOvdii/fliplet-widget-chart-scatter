1cb5483c5d721ce8974cb739724cc32ad7c30c0c
Andrey Riazantsev <71251809+AndrRyaz@users.noreply.github.com>
2020-11-04 16:11:37 +0200

    Addes access rules (#26)

--- js/interface.js
+++ js/interface.js
@@ -29,6 +29,15 @@ var dsQueryData = {
       }
     ]
   },
+  accessRules: [
+    {
+      allow: 'all',
+      enabled: true,
+      type: [
+        'select'
+      ]
+    }
+  ],
   result: data.dataSourceQuery
 };
 
